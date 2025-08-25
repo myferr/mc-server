b1d723c2d84f10c5beac23b3492285ae586c7926
myferr <contactme.myfer@protonmail.com>
2025-08-24 21:18:15 -0500

    fix(imagery): add 1.21.4 version and tag latest only for newest version

--- imagery/build.sh
+++ imagery/build.sh
@@ -2,13 +2,20 @@
 set -e
 
 # Versions in chronological order
-VERSIONS=("1.21.5" "1.21.6" "1.21.7" "1.21.8")
+VERSIONS=("1.21.4" "1.21.5" "1.21.6" "1.21.7" "1.21.8")
+
+LAST_INDEX=$((${#VERSIONS[@]} - 1))
+LAST_VERSION=${VERSIONS[$LAST_INDEX]}
 
 for VERSION in "${VERSIONS[@]}"; do
   echo "[imagery:info] Building mc-server:$VERSION ..."
 
-  # Generates image and pushes
-  ./imagery.sh "$VERSION" "mc-server-$VERSION" latest
+  # Tag 'latest' only for the newest version
+  if [ "$VERSION" == "$LAST_VERSION" ]; then
+    ./imagery.sh "$VERSION" "mc-server-$VERSION" latest
+  else
+    ./imagery.sh "$VERSION" "mc-server-$VERSION"
+  fi
 
   echo "[imagery:info] mc-server:$VERSION completed."
 done
